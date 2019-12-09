67bc4e12869b6cc65da94caa821bd00b10a63f83
Marco Gomiero <marco.gomiero.93@gmail.com>
2019-12-09 23:21:17 +0100

    Update gradle

--- core/src/main/java/com/prof18/filmatic/core/utils/Actions.kt
+++ core/src/main/java/com/prof18/filmatic/core/utils/Actions.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.content.Intent
 
 object Actions {
-    fun openHomeIntent(context: Context) = internalIntent(context, "com.prof18.filmatic.features.home")
+    fun openHomeIntent(context: Context): Intent =
+        internalIntent(context, "com.prof18.filmatic.features.home")
 
-    private fun internalIntent(context: Context, action: String) = Intent(action).setPackage(context.packageName)
+    private fun internalIntent(context: Context, action: String) =
+        Intent(action).setPackage(context.packageName)
 }
